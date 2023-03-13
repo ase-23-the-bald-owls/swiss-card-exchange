3e3fc0a98858d7ab5537a1c5dab418bdb14a8955
BacLuc <lucius.bachmann@gmx.ch>
2023-03-13 10:04:14 +0100

    app: add prettier and use eslint directly
    next lint does not have a check option for the ci

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,11 +1,11 @@
-import styles from '@/styles/Home.module.css'
+import styles from '@/styles/Home.module.css';
 
 export default function Home() {
   return (
     <>
       <main className={styles.main}>
-          <h1>Here comes the Swiss Card Exchange</h1>
+        <h1>Here comes the Swiss Card Exchange</h1>
       </main>
     </>
-  )
+  );
 }
