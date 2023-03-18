df13d572e388d79d003073be6a8fada0c76e14b1
BacLuc <lucius.bachmann@gmx.ch>
2023-03-19 00:28:53 +0100

    app: improve cypress config
    Add the color mode script that the page is rendered in dark mode.
    Reduce the viewport dimensions because the screenshots have anyway a
    max width of 1280 pixels (for now).
    
    Issue: #3

--- app/cypress.config.ts
+++ app/cypress.config.ts
@@ -10,6 +10,9 @@ export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000/',
     supportFile: 'cypress/support/e2e.ts',
+    // the screenshots dont work well with larger screens
+    viewportWidth: 1368,
+    viewportHeight: 768,
     setupNodeEvents(on, config) {
       // eslint-disable-next-line @typescript-eslint/no-var-requires
       require('@cypress/code-coverage/task')(on, config);
