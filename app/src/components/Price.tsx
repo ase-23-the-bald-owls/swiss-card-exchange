dc050892aeb6655d6476b2fdef6041b8f8bb77ee
BacLuc <lucius.bachmann@gmx.ch>
2023-03-20 16:47:59 +0100

    app: get price from database
    And add random prices to the seed data.
    
    Issue: #12
    Issue: #16

--- app/src/components/Price.tsx
+++ app/src/components/Price.tsx
@@ -1,10 +1,17 @@
 import { Text } from '@chakra-ui/react';
 
 type PriceProps = {
-  price: number;
+  price: number | null;
 };
 
 export function Price({ price }: PriceProps) {
+  if (price === null) {
+    return (
+      <Text color="blue.200" fontSize="2xl">
+        Price on request
+      </Text>
+    );
+  }
   return (
     <Text color="blue.200" fontSize="2xl">
       CHF {price.toFixed(2)}
