f3470cfdc6e3a5cf4a196642c171cf5963df190a
Off3line <johnyten@hotmail.com>
2023-03-20 17:00:23 +0100

    added modal when deleting a product

--- app/src/pages/backend.tsx
+++ app/src/pages/backend.tsx
@@ -1,8 +1,8 @@
 import { Product } from '@/lib/products';
 import styles from '@/styles/Home.module.css';
 import { createSupabaseServer } from '@/utils/supabase-server';
-import {Button, ButtonGroup, Input, InputGroup, InputLeftAddon, Stack, Table, TableCaption, TableContainer, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react';
-import { useState } from 'react';
+import {Button, ButtonGroup, Input, InputGroup, InputLeftAddon, Stack, Table, TableCaption, TableContainer, Tbody, Td, Th, Thead, Tr,Text, useDisclosure, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, FormControl, FormLabel, ModalFooter } from '@chakra-ui/react';
+import { useState,useRef } from 'react';
 import TableButton from '@/components/Table/TableButton';
 import { useProducts } from '@/hooks/useProducts';
 
@@ -26,9 +26,13 @@ export async function getServerSideProps() {
 export default function Backend({ products }: HomeProps) {
 
   const [editProduct, setEditProduct] = useState<Product | null >(null);
+  const [editId, setEditId] = useState<number | null>()
   const [editTitle,setEditTitle] = useState<string>('')
   const [editDesc, setEditDesc] = useState<string>('')
-  const [editId, setEditId] = useState<number | null>()
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const [deleteProduct,setDeleteProduct] = useState<Product | null>(null);
+  const initialRef = useRef(null)
+  const finalRef = useRef(null)
 
   const editEvent = (currentid:number):void => {
     
@@ -60,11 +64,28 @@ export default function Backend({ products }: HomeProps) {
    }
 
    const deleteEvent = (currentid:number):void => {
+      onOpen()
+      const selected = products.filter((product) => product.id === currentid)
+      
+      if (selected.length === 0) {
+
+        throw new Error('product not found lol')
+      }
+      console.log(selected);
+      
+      setDeleteProduct(selected[0] || null)
     
     console.log('id selected: ',currentid)
  }
 
+ 
+  const handleDeleteConfirm = ():void => {
 
+    console.log(deleteProduct, 'deleted!');
+    
+    
+    onClose()
+  }
   const clearEvent =():void => { 
     
     setEditProduct(null)
@@ -84,17 +105,13 @@ export default function Backend({ products }: HomeProps) {
     <>
       <main className={styles.main} data-testid="index">
         <h1>Product Backend</h1>
-        <Stack direction='row' spacing={2} align='center'>
+        <Stack direction='row' spacing={5} align='center'>
+        <Text>id: {editId}</Text>
         <InputGroup size='sm'>
-          <InputLeftAddon children="id"/>
-          <Input type="text" borderLeftRadius="0" value={editId} isDisabled={true}/>
-
           <InputLeftAddon children="title"/>
           <Input type="text" borderLeftRadius="0" value={editTitle} onChange={(event) => handleTitleChange(event.target.value)}/>
-
           <InputLeftAddon children='description'/>
           <Input type='text' borderLeftRadius='0' value={editDesc} onChange={(event) => handleDescChange(event.target.value)}/>
-
         </InputGroup>
         <ButtonGroup>
           <Button colorScheme="teal" variant="outline" onClick={() => handleSave()}>
@@ -130,6 +147,29 @@ export default function Backend({ products }: HomeProps) {
           <TableCaption>Product Backlog</TableCaption>
           </Table>
         </TableContainer>
+
+        <Modal initialFocusRef={initialRef} finalFocusRef={finalRef} isOpen={isOpen}onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Are you sure you want to the delete the following product?</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <FormControl mt={4}>
+              <FormLabel>id: {deleteProduct?.id}</FormLabel>
+              <FormLabel>Title: {deleteProduct?.title}</FormLabel>
+              <FormLabel>Description: {deleteProduct?.description}</FormLabel>
+              <FormLabel>Rarity: {deleteProduct?.rarity}</FormLabel>
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme='red' mr={3} onClick={()=> handleDeleteConfirm()}>
+              Delete
+            </Button>
+            <Button onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
       </main>
     </>
   );
