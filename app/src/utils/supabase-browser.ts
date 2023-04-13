4dc954145c98c1bc5ca659e9755674d594dfed19
BacLuc <lucius.bachmann@gmx.ch>
2023-04-13 13:50:38 +0200

    app: use env variables at runtime for supabaseBrowser
    We still need them at build time because supabase checks
    if these parameters are set.
    With this hack in the entrypoint they are replaced
    at runtime.
    In the webpack build output, it replaces a string like:
    supabaseUrl:t=a.NEXT_PUBLIC_SUPABASE_URL
    with
    supabaseUrl:t="http://localhost:8080"
    
    Issue: #23

--- app/src/utils/supabase-browser.ts
+++ app/src/utils/supabase-browser.ts
@@ -4,4 +4,10 @@
 import { Database } from '../lib/database.types';
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
 
-export const createSupabaseBrowser = () => createBrowserSupabaseClient<Database>();
+// store as const here that they don't get inlined at build time
+const runtimeEnv = process.env;
+export const createSupabaseBrowser = () =>
+  createBrowserSupabaseClient<Database>({
+    supabaseUrl: runtimeEnv.NEXT_PUBLIC_SUPABASE_URL,
+    supabaseKey: runtimeEnv.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  });
