aa7a66e633acda39f78c8a78d7e9014b5b4cd7fb
Philipp WSL <johnyten@hotmail.com>
2023-04-15 17:19:19 +0200

    hotfix

--- mail-function/src/order-mailer.js
+++ mail-function/src/order-mailer.js
@@ -4,7 +4,7 @@ const nodemailer = require('nodemailer')
 
 
 // Create a single supabase client for interacting with your database
-const supabase = spb.createClient('http://localhost:54321','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0')
+const supabase = spb.createClient()
 
 const recipients = []
 const orders = []
