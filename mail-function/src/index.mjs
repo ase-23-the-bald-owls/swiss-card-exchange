90130e79b60196d1f19c212d69d3015749d69c30
Off3line <johnyten@hotmail.com>
2023-04-24 14:43:57 +0200

    mail-function: dockerize

--- mail-function/src/index.mjs
+++ mail-function/src/index.mjs
@@ -8,7 +8,7 @@ config();
 // Create a single supabase client for interacting with your database
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY
 );
 
 const dbData = await loadDBData();
