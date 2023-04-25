c34d214f724c472a9fd6cb41aaafc11c19aff839
BacLuc <lucius.bachmann@gmx.ch>
2023-04-25 09:33:49 +0200

    app: improve the next build in the docker image
    Only include node_modules from the deps step.
    Remove test files and testutils.
    Use a dedicated SUPABASE env config for build.

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -5,7 +5,19 @@ set -e
 if [ "$1" != "default" ]; then
   exec "$@"
 fi
+
+if [ "$NEXT_PUBLIC_SUPABASE_URL" = "$BUILD_SUPABASE_URL" ] \
+   || [ "$NEXT_PUBLIC_SUPABASE_ANON_KEY" = "$BUILD_SUPABASE_ANON_KEY" ]; then
+  echo "You must specify NEXT_PUBLIC_SUPABASE_URL and NEXT_PUBLIC_SUPABASE_ANON_KEY"
+  echo "NEXT_PUBLIC_SUPABASE_URL: $NEXT_PUBLIC_SUPABASE_URL"
+  echo "NEXT_PUBLIC_SUPABASE_ANON_KEY: $NEXT_PUBLIC_SUPABASE_ANON_KEY"
+  exit 1
+fi
+
 for env_var in "${!NEXT_PUBLIC@}"; do
+  sed_command="s|process\\.env\\.$env_var|\"${!env_var}\"|g"
+  find /app/.next/ -type f -print0 | xargs -0 sed -i "$sed_command"
+
   sed_command="s|[a-zA-Z][a-zA-Z0-9]*\\.$env_var|\"${!env_var}\"|g"
   find /app/.next/static -type f -print0 | xargs -0 sed -i "$sed_command"
 done
