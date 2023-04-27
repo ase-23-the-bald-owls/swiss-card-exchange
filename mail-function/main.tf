d7d3367e67cbf675f225e979dc4185fc75aac28d
Philipp WSL <johnyten@hotmail.com>
2023-04-27 23:40:37 +0200

    temp changes to build and use the dockerfile, not working

--- mail-function/main.tf
+++ mail-function/main.tf
@@ -10,13 +10,20 @@ data "archive_file" "archive_lambda_mail_function" {
   output_path = "${path.module}/mail-function.zip"
 }
 
+resource "docker_image" "mail_docker_service"{
+ name = "mail-function-service"
+ source_dir = path.module
+ image = "${path.module}/Dockerfile"
+
+}
 resource "aws_s3_object" "mail_function_object" {
   bucket = aws_s3_bucket.cronJob-bucket.id
 
-  key    = "mail-function.zip"
-  source = data.archive_file.archive_lambda_mail_function.output_path
+  //key    = "mail-function.zip"
+  //source = data.archive_file.archive_lambda_mail_function.output_path
+  source = docker_image.mail_docker_service.name
 
-  etag = filemd5(data.archive_file.archive_lambda_mail_function.output_path)
+ // etag = filemd5(data.archive_file.archive_lambda_mail_function.output_path)
 }
 
 resource "aws_lambda_function" "mail_function" {
@@ -25,13 +32,15 @@ resource "aws_lambda_function" "mail_function" {
   s3_bucket = aws_s3_bucket.cronJob-bucket.id
   s3_key    = aws_s3_object.mail_function_object.key
 
+  image_uri = docker_image.mail_docker_service.image
   runtime = "nodejs16.x"
   handler = "mail.handler"
 
-  source_code_hash = data.archive_file.archive_lambda_mail_function.output_base64sha256
+  //source_code_hash = data.archive_file.archive_lambda_mail_function.output_base64sha256
 
   role = aws_iam_role.lambda_exec.arn
 }
+
 
 resource "aws_cloudwatch_log_group" "mail_cloudwatch" {
   name = "/aws/lambda/${aws_lambda_function.mail_function.function_name}"
