99646968372e84fa0e44591ea7ba4372616f2061
BacLuc <lucius.bachmann@gmx.ch>
2023-04-29 13:07:31 +0200

    app: add row level security on order
    That users can only see their own orders, and anonymous users can
    see the orders (and dependent rows of other entities) they created.
    
    Issue: #11

--- app/src/lib/addresses.ts
+++ app/src/lib/addresses.ts
@@ -1,5 +1,5 @@
 import { Database } from '@/lib/database.types';
 
 export type Address = Database['public']['Tables']['addresses']['Row'];
-export type AddressWithId = Address;
-export type AddressWithoutId = Omit<Address, 'id'>;
+export type AddressWithId = Omit<Address, 'creator_expiry_time'>;
+export type AddressWithoutId = Omit<Address, 'id' | 'creator_expiry_time'>;
