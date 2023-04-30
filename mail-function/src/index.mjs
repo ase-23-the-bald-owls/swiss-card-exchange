89aa56c5738a7534ebadde16f712603d21fdd224
Philipp WSL <johnyten@hotmail.com>
2023-04-30 17:14:55 +0200

    small fix

--- mail-function/src/index.mjs
+++ mail-function/src/index.mjs
@@ -66,7 +66,7 @@ async function notificationMail(orders) {
       html: `<b>Dear Customer, thanks for order with the following info: order nr. ${orderId}</b>`,
     });
     let time = new Date();
-    console.log('Message sent: %s', info.messageId,  ' with time: ', time.toLocaleDateString());
+    console.log('Message sent: %s', info.messageId,  ' with time: ', time.getHours(),time.getMinutes(),time.getSeconds());
     // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
     console.log(info.response);
